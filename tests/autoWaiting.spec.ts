40c8a5135011c43b83d3cc4ad0581cd06349bb4c
Harrison Lawrence <harrison.lawrence@wrenkitchens.com>
2025-12-17 13:39:20 +0000

    Added new test cases for auto-waiting and UI components

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -11,3 +11,23 @@ test("Auto-waiting", async ({ page }) => {
   const text = successButton.textContent();
   expect(text).toEqual("Data loaded with AJAX get request.");
 });
+
+test("alternative waits", async ({ page }) => {
+  const successButton = page.locator(".bg-success");
+  // wait for element
+  await page.waitForSelector(".bg-success");
+  // wait for particlular response
+  await page.waitForResponse("http://uitestingplayground.com/ajaxdata");
+
+  // wait for network calls to be completed ('NOT RECOMMENDED' )
+  await page.waitForLoadState("networkidle");
+  const text = await successButton.allTextContents();
+  expect(text).toContain("Data loaded with AJAX get request.");
+});
+
+test("timeouts", async ({ page }) => {
+  test.setTimeout(10000);
+  test.slow();
+  const successButton = page.locator(".bg-success");
+  await successButton.click();
+});
